818227125ffcb9ea8702e4f888e2a6b99d523491
VasilGVasilev <vaskomeil@gmail.com>
2022-02-12 12:36:29 +0200

    scrapper ready

--- server/scrapers.js
+++ server/scrapers.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
-url = 'https://www.luximmo.bg/'
 
-async function Scraper(url) {
+async function scrapeChannel(url) {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto(url);
@@ -17,29 +16,4 @@ async function Scraper(url) {
     
   };
 
-  Scraper('https://dev.bg/?s=junior&post_type=job_listing')  
-// const puppeteer = require('puppeteer');
-
-// async function scrapeChannel(url) {
-
-//     //open browser, create a new page, navigate to url
-//     const browser = await puppeteer.launch({headless: false});
-//     const page = await browser.newPage();
-//     await page.goto(url);
-//     await page.click('#cookie-bar > p > a.cb-enable.button.small.block')
-
-//     //use deconstructor to pull out first item this func returns, extract text, stringify
-
-//     const [el] = await page.$x('/html/body/div[4]/div[6]/div[2]/div[1]/div/p')
-//     const text = await el.getProperty('textContent');
-//     const name = await text.jsonValue();
-
-//     browser.close(); //to not pend forever
-    
-//     console.log({name});
-
-//     return {name};
-
-// }
-
-// scrapeChannel('https://www.luximmo.bg/')
+scrapeChannel('https://dev.bg/?s=junior&post_type=job_listing')  
